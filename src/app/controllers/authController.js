73cefb5c96995f197ec4a5b81b50a783ad31c36c
doravantebeto <roberto.araujo339@gmail.com>
2021-03-22 01:43:32 -0300

    Fixed import

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -3,9 +3,9 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 require('dotenv/config');
 
-const secret = process.env.SECRET
+const secret = process.env.SECRET;
 
-const User = require('../models/User');
+const User = require('../models/user');
 
 const router = express.Router();
 
