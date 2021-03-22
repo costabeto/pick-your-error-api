72e5239e4113dcc6d6ec68ccee6f26fe29f474db
doravantebeto <roberto.araujo339@gmail.com>
2021-03-22 01:41:32 -0300

    Removed body parser

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 require('dotenv/config');
 
 const app = express();
 
 app.use(cors());
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 require('./app/controllers/index')(app);
 
 const port = process.env.PORT || 3333;
 
-app.listen(port, console.log(`Listening on http://localhost:3333`));
+app.listen(port, console.log(`Listening on http://localhost:${port}`));
